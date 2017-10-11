e7ad4f3952a58e6694999b8ce7f14d126d2955f8
Juan David Villena Gil <2smrdavid@gmail.com>
2017-10-11 11:11:35 +0200

    He añadido el primer ejercicio

---
+++ ejercicios0405/ejercicio1t4.5.java
@@ -0,0 +1,12 @@
+/**
+*Ejercicios tema4
+*Juan David Villena Gil
+*Escribe un programa que pida por teclado un día de la semana y que diga qué
+asignatura toca a primera hora ese día.
+*/
+public class ejercicio1t4.5
+	public static void main(String[] args) {
+    System.out.print("Introduzca el dia de la semana: ");
+    String semana = System.console().readLine();
+  }
+}
